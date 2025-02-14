3d41a311fd5a80d27332d0c91887e52c17f571a7
lampros alexandris <lamprospatmos19@gmail.com>
2025-02-15 01:47:11 +0200

    import & export

---
+++ modules/src/lib1.js
@@ -0,0 +1,10 @@
+export const PI = 3.14
+export const EPSILON = 0.00005
+export const API_URL = "https://codingfactory.aueb.gr"
+
+//defult export - only one defult
+const SEC_PER_MIN = 60;
+export default  SEC_PER_MIN
+
+
+
---
+++ modules/src/lib2.js
@@ -0,0 +1,9 @@
+const PI = 3.14
+const EPSILON = 0.00005
+const API_URL = "https://codingfactory.aueb.gr"
+
+//defult export - only one defult
+const SEC_PER_MIN = 60;
+export default  SEC_PER_MIN
+
+export {PI, EPSILON, API_URL}
