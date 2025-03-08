f50a946b7de499e0cfef375d875799f0e0c4083e
lampros alexandris <lamprospatmos19@gmail.com>
2025-03-08 23:01:46 +0200

    movies-app project

---
+++ apps/movies-app/js/index.js
@@ -0,0 +1,13 @@
+$(function() {
+
+    let debounceTimeout = null
+    $('#searchInput').on('input', function(){
+        clearTimeout(debounceTimeout)
+        debounceTimeout = setTimeout(() => getMovie(this.value.trim()), 1500)
+    })
+
+    $('#showMore').on('click', function(e){
+        e.preventDefault();
+        onShowMoreClicked();
+    })
+})
