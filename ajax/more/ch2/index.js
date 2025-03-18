346b29c413a2c8b05bd4e3683b982268c0914f9d
lampros alexandris <lamprospatmos19@gmail.com>
2025-03-19 01:47:53 +0200

    axios, fetch ip , data in array

--- ajax/more/ch2/index.js
+++ ajax/more/ch2/index.js
@@ -1,16 +1,21 @@
-let url = 'data.json'
+const url = "data.json";
 
-document.getElementById("btn").addEventListener('click', async () => {
-    try {
-        let response = await fetch(url)
-        if(!response.ok)throw new Error('Invalid response');
-        let data = await response.json();
-        console.log("fetch data: ", data)
-        document.getElementById('output').innerHTML = `<b>Firstname:</b> ${data[0].firstname}
-                                                       <br>
-                                                      <b>Lastname:</br> ${data[0].lastname}`
-    } catch (error){
-        console.error('Fetch error', error)
-        document.getElementById('output').innerHTML = 'Error fetching data'
+document.getElementById('btn').addEventListener('click', async() => {
+    try{
+    let response = await fetch(url)
+    if(!response.ok) throw new Error ("error data");
+    let data = await response.json();
+    console.log("Fetch data: " , data);
+
+    document.getElementById('output').innerHTML = `<b>Data:</b>
+                                                <br>
+                                            firstname:${data[0].firstname},
+                                            lastname:${data[0].lastname}
+                                             `
+
+    }catch(error){
+    console.error(error);
+     document.getElementById('output').innerHTML = 'Error fetching data'
     }
+    
 })
