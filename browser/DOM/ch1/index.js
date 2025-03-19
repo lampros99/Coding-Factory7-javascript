3e0292e4d5d95a30457daeed3b33735a382b049a
lampros alexandris <lamprospatmos19@gmail.com>
2025-03-19 03:09:55 +0200

    traverse DOM

---
+++ browser/DOM/ch1/index.js
@@ -0,0 +1,15 @@
+// document.firstElementChild.lastElementChild.querySelector('ul').lastElementChild.innerHTML ='hi'
+
+// document.firstElementChild.querySelector('h1').innerHTML= 'hi';
+
+// document.firstElementChild.querySelector('input').click();
+
+// document.firstElementChild.lastElementChild.querySelector('li').style = 'color:red'; 
+
+document.querySelector('h1').innerHTML = 'lampros';
+
+document.getElementById('btn').style = 'color:blue';
+
+document.querySelector('#list').style = 'color:pink';
+
+document.querySelector('#list li a').style.textDecoration = 'none';
--- browser/DOM/index.js
+++
@@ -1,7 +0,0 @@
-document.firstElementChild.lastElementChild.querySelector('ul').lastElementChild.innerHTML ='hi'
-
-document.firstElementChild.querySelector('h1').innerHTML= 'hi';
-
-document.firstElementChild.querySelector('input').click();
-
-document.firstElementChild.lastElementChild.querySelector('li').style = 'color:red'; 
