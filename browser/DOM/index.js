59f163dac86061b12c71aa38298b14e587425c2b
lampros alexandris <lamprospatmos19@gmail.com>
2025-03-19 02:39:02 +0200

    traverse DOM

---
+++ browser/DOM/index.js
@@ -0,0 +1,7 @@
+document.firstElementChild.lastElementChild.querySelector('ul').lastElementChild.innerHTML ='hi'
+
+document.firstElementChild.querySelector('h1').innerHTML= 'hi';
+
+document.firstElementChild.querySelector('input').click();
+
+document.firstElementChild.lastElementChild.querySelector('li').style = 'color:red'; 
