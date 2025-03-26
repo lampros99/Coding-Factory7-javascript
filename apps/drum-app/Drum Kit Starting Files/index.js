f9ee6dcc28c6e4d89b5d45b222baaef1902c5d65
lampros alexandris <lamprospatmos19@gmail.com>
2025-03-26 23:11:08 +0200

    funny drum game

---
+++ apps/drum-app/Drum Kit Starting Files/index.js
@@ -0,0 +1,40 @@
+const numberOfDrums = document.querySelectorAll('.drum').length;
+
+for(let i = 0; i < numberOfDrums; i++){
+
+document.querySelectorAll('.drum')[i].addEventListener('click', function(){
+   let buttonEvent = this.innerHTML;
+
+    switch(buttonEvent){
+        case 'w':
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case 'a':
+            let tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case 's':
+            let tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case 'd':
+            let tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case 'j':
+        const crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+        case 'k':
+        const kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+        case 'l':
+        const snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+    }
+})
+}
+    
